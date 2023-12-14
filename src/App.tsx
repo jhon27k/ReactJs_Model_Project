d39022f145522e853788915a699cb3323cccd313
jhon27k <jonathas.ec14@gmail.com>
2023-12-13 20:59:52 -0400

    first

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello</h1>
+      <Button variant="primary"></Button>
+      <Button variant="secundary"></Button>
+      <Button variant="danger"></Button>
+      <Button variant="primary"></Button>
+      <Button></Button>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
